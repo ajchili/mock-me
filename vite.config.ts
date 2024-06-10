1ff81dd43f8a38dcc682089acc5ca4788f0f6a64
Kirin Patel <kirinpatel@gmail.com>
2024-06-10 15:00:53 -0400

    🔧 setup HMR and add some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({});
+export default defineConfig({
+	mode: 'development',
+  plugins: [react()],
+});
