517e4bf7d66f1b55cdff0a99acb7e8e56b6edcf7
Kirin Patel <kirinpatel@gmail.com>
2024-06-10 15:15:53 -0400

    🤩 launch website on vite start

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	mode: 'development',
+  mode: "development",
   plugins: [react()],
+  server: {
+    open: "/index.html",
+  },
 });
