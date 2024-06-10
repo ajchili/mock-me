dbdad8e348eca204633a63c044d9be8c200f44f9
Kirin Patel <kirinpatel@gmail.com>
2024-06-10 15:15:32 -0400

    🗑️ remove contexts for now

--- src/state/context/person.tsx
+++
@@ -1,7 +0,0 @@
-import { Provider, createContext } from "react";
-
-export type Person = "candidate" | "interviewer";
-
-const PersonContext = createContext<Person | undefined>(undefined);
-
-const fart = (): JSX.Element => <PersonContext.Provider />;
