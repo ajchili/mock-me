745cf3da8da1abbcdcd2396c870275a3e41dd1fc
Kirin Patel <kirinpatel@gmail.com>
2024-06-11 15:09:14 -0400

    🔧 get asset DockerFile built (kinda)
    HMR broke

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -1,14 +1,21 @@
 import { SelfContainedImageRepository } from "./components/SelfContainedImageRepository";
 
+const assetImageRepository = new SelfContainedImageRepository(
+  "asset-image-repository",
+  {
+    image: {
+      name: "asset",
+    },
+  }
+);
 const serverImageRepository = new SelfContainedImageRepository(
   "server-image-repository",
   {
     image: {
       name: "server",
-      context: "../server",
-      dockerfile: "../server/Dockerfile",
     },
   }
 );
 
-export const imageName = serverImageRepository.image.imageName;
+export const assetImageName = assetImageRepository.image.imageName;
+export const serverImageName = serverImageRepository.image.imageName;
