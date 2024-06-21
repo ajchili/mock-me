8f0e9040e44199a0b3dc52c005ca6d5b954819f1
Kirin Patel <kirinpatel@gmail.com>
2024-06-20 23:50:20 -0400

    🔧 fix webworker loading issues

--- packages/asset/src/components/Editor/Editor.tsx
+++ packages/asset/src/components/Editor/Editor.tsx
@@ -2,6 +2,30 @@ import { useRef, useState, useEffect } from "react";
 import * as monaco from "monaco-editor";
 import { useStyles } from "./styles.js";
 
+// https://github.com/microsoft/monaco-editor/issues/2122#issuecomment-898307500
+
+import "monaco-editor/esm/vs/basic-languages/css/css.contribution";
+import "monaco-editor/esm/vs/basic-languages/xml/xml.contribution";
+import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution";
+
+import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.MonacoEnvironment = {
+  getWorker(_: string, label: string) {
+    if (label === "typescript" || label === "javascript") return new TsWorker();
+    if (label === "json") return new JsonWorker();
+    if (label === "css") return new CssWorker();
+    if (label === "html") return new HtmlWorker();
+    return new EditorWorker();
+  },
+};
+
 export interface EditorProps {
   value: string;
   onChange?: (event: monaco.editor.IModelContentChangedEvent) => void;
