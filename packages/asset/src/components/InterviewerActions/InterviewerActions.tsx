299590697ceda443b27255e188c1cf38d4a70b03
Kirin Patel <kirinpatel@gmail.com>
2024-07-14 18:54:33 -0400

    🤩 add LeetCode question selection

---
+++ packages/asset/src/components/InterviewerActions/InterviewerActions.tsx
@@ -0,0 +1,72 @@
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+
+import { Button } from "../Button/Button.js";
+
+export const InterviewerActions = () => {
+  const writeToDoc = (room: string, content: string) => {
+    const ydoc = new Y.Doc();
+    const { hostname } = window.location;
+    const provider = new WebsocketProvider(`ws://${hostname}:1234`, room, ydoc);
+
+    ydoc.getText("monaco").delete(0, ydoc.getText("monaco").length);
+    ydoc.getText("monaco").insert(0, content);
+    // Disconnect after assuming that delete and insert were successful
+    setTimeout(() => provider.destroy(), 1000);
+  };
+
+  const loadQuestion = async (titleSlug: string) => {
+    const response = await fetch(
+      `http://${window.location.hostname}:3000/select?titleSlug=${titleSlug}`
+    );
+    const { difficulty, question } = await response.json();
+
+    writeToDoc("response", "function solution() {\n\t\n}");
+    writeToDoc(
+      "prompt",
+      `<div class="flex gap-1"><a href="https://leetcode.com/problems/${titleSlug}" target="_blank">${titleSlug}</a><span class="difficulty-badge ${difficulty.toLowerCase()}">${difficulty}</span></div><hr/>${question}`
+    );
+  };
+
+  const selectDailyQuestion = async () => {
+    const response = await fetch(
+      `http://${window.location.hostname}:3000/dailyQuestion`
+    );
+    const { data } = await response.json();
+
+    await loadQuestion(
+      data.activeDailyCodingChallengeQuestion.question.titleSlug
+    );
+  };
+
+  const selectQuestion = async () => {
+		const titleSlug = prompt("Select LeetCode question. Please provide the slug (e.g. \"two-sum\").");
+
+		if (titleSlug) {
+			await loadQuestion(titleSlug);
+		}
+  };
+
+  const selectRandomQuestion = async () => {
+    // Pending: https://github.com/alfaarghya/alfa-leetcode-api/pull/27
+    const response = await fetch(
+      `http://${
+        window.location.hostname
+      }:3000/problems?limit=2&skip=${Math.floor(Math.random() * 3219)}`
+    );
+    const { problemsetQuestionList } = await response.json();
+
+    await loadQuestion(problemsetQuestionList[0].titleSlug);
+  };
+
+  return (
+    <div className="flex w-full flex-1 flex-col gap-4 overflow-y-auto px-4 py-5">
+      <div>
+        <h1 className="text-xl">LeetCode</h1>
+        <Button onClick={selectDailyQuestion}>Select Daily Question</Button>
+        <Button onClick={selectRandomQuestion}>Select Random Question</Button>
+        <Button onClick={selectQuestion}>Select Question by Title Slug</Button>
+      </div>
+    </div>
+  );
+};
--- packages/asset/src/pages/Interviewer/Interviewer.tsx
+++ packages/asset/src/pages/Interviewer/Interviewer.tsx
@@ -1,48 +1,9 @@
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-
 import { Editor } from "../../components/Editor/Editor.js";
 import { TabPane } from "../../components/TabPane/TabPane.js";
 import { Prompt } from "../../components/Prompt/Prompt.js";
-import { Button } from "../../components/Button/Button.js";
+import { InterviewerActions } from "../../components/InterviewerActions/InterviewerActions.js";
 
 export const Interviewer = (): JSX.Element => {
-  const writeToDoc = (room: string, content: string) => {
-    const ydoc = new Y.Doc();
-    const { hostname } = window.location;
-    const provider = new WebsocketProvider(`ws://${hostname}:1234`, room, ydoc);
-
-    ydoc.getText("monaco").delete(0, ydoc.getText("monaco").length);
-    ydoc.getText("monaco").insert(0, content);
-    // Disconnect after assuming that delete and insert were successful
-    setTimeout(() => provider.destroy(), 1000);
-  };
-
-  const selectDailyQuestion = async () => {
-    const response = await fetch(
-      `http://${window.location.hostname}:3000/dailyQuestion`
-    );
-    const { data } = await response.json();
-    const { question } = data.activeDailyCodingChallengeQuestion;
-
-    writeToDoc(
-      "response",
-      question.codeSnippets.find(
-        ({ langSlug }: any) => langSlug === "typescript"
-      )?.code
-    );
-    writeToDoc(
-      "prompt",
-      `<div class="flex gap-1"><a href="https://leetcode.com/problems/${
-        question.titleSlug
-      }" target="_blank">${
-        question.titleSlug
-      }</a><span class="difficulty-badge ${question.difficulty.toLowerCase()}">${
-        question.difficulty
-      }</span></div><hr/>${question.content}`
-    );
-  };
-
   return (
     <div className="flex w-full h-dvh">
       <Editor
@@ -69,13 +30,7 @@ export const Interviewer = (): JSX.Element => {
             },
             {
               title: "Interviewer Actions",
-              children: (
-                <div>
-                  <Button onClick={selectDailyQuestion}>
-                    Select Daily Question
-                  </Button>
-                </div>
-              ),
+              children: <InterviewerActions />,
             },
           ]}
         />
