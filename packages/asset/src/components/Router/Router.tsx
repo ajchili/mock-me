346b6207e5b31f3ffa26402fa4d22d1f1ed0e98c
Kirin Patel <kirinpatel@gmail.com>
2024-09-18 20:58:17 -0400

    🔧 use browser router

--- packages/asset/src/components/Router/Router.tsx
+++ packages/asset/src/components/Router/Router.tsx
@@ -1,13 +1,12 @@
 import {
   createBrowserRouter,
-  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import { Login } from "../../pages/Login/Login.js";
 import { Interviewer } from "../../pages/Interviewer/Interviewer.js";
 import { Candidate } from "../../pages/Candidate/Candidate.js";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   { path: "/", Component: Login },
   { path: "/interviewer", Component: Interviewer },
   { path: "/candidate", Component: Candidate },
