47003a5d0d0806fdf0eed9e5b46968ac953964d1
Kirin Patel <kirinpatel@gmail.com>
2024-09-18 21:36:06 -0400

    🔧 fix leetcode integration

--- packages/asset/src/utils/http.ts
+++ packages/asset/src/utils/http.ts
@@ -1,6 +1,7 @@
 export const buildEndpoint = (protocol: "http" | "ws") => {
   const isSecureConnection = window.location.protocol === "https:";
+  const path = protocol === "http" ? "" : "/ws"
   return `${isSecureConnection ? `${protocol}s` : protocol}://${
     window.location.host
-  }/ws`;
+  }${path}`;
 };
