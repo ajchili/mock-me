f7a3dc2ab49a85b18581ae3f149aa82a06982a42
Kirin Patel <kirinpatel@gmail.com>
2024-11-08 23:40:39 -0500

    🗑️remove infrastructure package

--- packages/infrastructure/index.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-
-import { App } from "./components/App";
-
-const app = new App("mock-me");
-
-export const url = pulumi.interpolate`http://${app.loadbalancer.dnsName}`;
