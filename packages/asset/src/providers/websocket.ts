0e82e5395ef372295bf6b9f85aaef9fb4eaac18d
Kirin Patel <kirinpatel@gmail.com>
2024-11-12 23:16:11 -0500

    🗑️ remove buildEndpoint util

--- packages/asset/src/providers/websocket.ts
+++ packages/asset/src/providers/websocket.ts
@@ -1,7 +1,5 @@
 import { type Doc } from "yjs";
 import { WebsocketProvider } from "y-websocket";
 
-import { buildEndpoint } from "../utils/http.js";
-
 export const buildWebsocketProvider = (roomName: string, doc: Doc) =>
-  new WebsocketProvider(buildEndpoint("ws"), roomName, doc);
+  new WebsocketProvider("/ws", roomName, doc);
--- packages/asset/src/utils/http.ts
+++
@@ -1,7 +0,0 @@
-export const buildEndpoint = (protocol: "http" | "ws") => {
-  const isSecureConnection = window.location.protocol === "https:";
-  const path = protocol === "http" ? "" : "/ws"
-  return `${isSecureConnection ? `${protocol}s` : protocol}://${
-    window.location.host
-  }${path}`;
-};
